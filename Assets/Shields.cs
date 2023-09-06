911520c1ca515e4f1ad5eb27f2e7956f84e35a78
JaBaconator <99735923+JaBaconator@users.noreply.github.com>
2023-09-06 11:21:52 -0400

    Class 2
    Still working on the ray shields.

--- Assets/Shields.cs
+++ Assets/Shields.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Shields : MonoBehaviour {
+    Trigger trigger = new Trigger();
+
     // Start is called before the first frame update
     void Start() {
         
@@ -10,6 +12,6 @@ public class Shields : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (onTriggerEnter) { }
+        trigger OnTriggerEnter;
     }
 }
